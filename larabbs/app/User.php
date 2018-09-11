7693df17f3cc8abdb26b23caa0119c5bc9a78c0f
Demoxy <846434796@qq.com>
2018-09-11 11:34:09 +0800

    修改

--- larabbs/app/User.php
+++ larabbs/app/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -15,11 +15,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','email_verified'
-    ];
-
-    protected $casts = [
-        'email_verified' => 'boolean',
+        'name', 'email', 'password',
     ];
 
     /**
--- larabbs/routes/web.php
+++ larabbs/routes/web.php
@@ -10,24 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-//
-//Route::get('/', function () {
-//    return view('welcome');
-//});
-//
-Route::get('/', 'PagesController@root');
 
-Auth::routes();
-
-//Route::get('/home', 'HomeController@index')->name('home');
-Route::group(['middleware' => 'auth'], function() {
-    Route::get('/email_verify_notice', 'PagesController@emailVerifyNotice')->name('email_verify_notice');
-
-    // 开始
-    Route::group(['middleware' => 'email_verified'], function() {
-        Route::get('/test', function() {
-            return 'Your email is verified';
-        });
-    });
-    // 结束
+Route::get('/', function () {
+    return view('welcome');
 });
